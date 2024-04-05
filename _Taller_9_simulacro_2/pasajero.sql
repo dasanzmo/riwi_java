05c148be0d8e79a6c220d0114e04e780e629cd8d
danielsm <danielsanchezm92@gmail.com>
2024-04-05 17:23:28 -0500

    Carpeta java reordenada

--- _Taller_9_simulacro_2/pasajero.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM aerolinea.pasajero;
-
-INSERT INTO pasajero(nombre, apellido, documento_identidad) VALUES 
-("Daniel","Sanchez","1146435560"),
-("Susana","Duque","1037666687"),
-("Juan","Sierra","8028336"),
-("Sofía", "Morales","43510365");
